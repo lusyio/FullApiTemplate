ac0cd8f674636b58093747f0e70a834bbe5f47cc
adim <apple@adimMac.mshome.net>
2024-02-06 10:09:29 +0300

    Finalized

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -10,7 +10,7 @@ async function handleLogout(){
     <ul class=" bg-orange-100 flex flex-grow justify-center">
       <li><nuxt-link to="/">Home</nuxt-link></li>
       <li><nuxt-link to="/auth/dashboard">Dashboard</nuxt-link></li>
-      <li><nuxt-link to="/guest/register">Register</nuxt-link></li>
+      <li v-if="!auth.isLoggedIn"><nuxt-link to="/guest/register">Register</nuxt-link></li>
       <li  v-if="!auth.isLoggedIn"><nuxt-link to="/guest/login">Login</nuxt-link></li>
       <li v-if="auth.isLoggedIn"  @click.prevent="handleLogout"><nuxt-link to="/#">Logout</nuxt-link></li>
     </ul>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+      head: {
+          "meta": [
+            {
+              "name": "viewport",
+              "content": "width=device-width, initial-scale=1"
+            },
+            { "charset": "utf-8"}
+          ],
+          "link": [],
+          "style": [],
+          "script": [],
+          "noscript": []
+        }
+  },
   modules: [
     // ...
     '@pinia/nuxt',
