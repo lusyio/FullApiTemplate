2eeb08f1e385572d55b14022238829afbc3174a1
Ark21bit <webleif@vk.com>
2024-03-18 17:58:20 +0300

    nuxt config meta delete && typedPages add

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,20 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {enabled: true},
-    app: {
-        head: {
-            'meta': [
-                {
-                    'name': 'viewport',
-                    'content': 'width=device-width, initial-scale=1'
-                },
-                {'charset': 'utf-8'}
-            ],
-            'link': [],
-            'style': [],
-            'script': [],
-            'noscript': []
-        }
+    devtools: { enabled: true },
+    experimental: {
+        typedPages: true
     },
     modules: [
         // ...
